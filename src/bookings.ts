a73f5599fa20ec9fa5557d041d34f5a001399ca1
Lisa Lemons <lisalemons@Lisas-MBP-2.hsd1.mi.comcast.net>
2020-10-06 15:07:05 -0400

    lesson 3.2

--- src/bookings.ts
+++
@@ -1,9 +0,0 @@
-export interface Booking {
-  id: string;
-  title: string;
-  image: string;
-  address: string;
-  timestamp: string;
-}
-
-export const bookings: Booking[] = [];
--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,13 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { Listing,listings } from './listings';
-import { Booking, bookings } from './bookings';
+import {ApolloServer} from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
+const server = new ApolloServer({ schema });
+server.applyMiddleware({app, path: '/api'});
 
-// Listings
-app.get('/listings', (_req, res) => {
-  res.send(listings);
-});
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1)[0]);
-    }
-  }
-
-  return res.send('failed to deleted listing');
-});
-
-// Booking
-app.post('/create-booking', (req, res) => {
-  const id: string = req.body.id;
-  const timestamp = req.body.timestamp || new Date().toLocaleString();
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      const newBooking: Booking = {
-        id: bookings.length.toString(),
-        title: listings[i].title,
-        image: listings[i].image,
-        address: listings[i].address,
-        timestamp
-      };
-      bookings.push(newBooking);
-      listings[i].bookings.push(newBooking.id);
-
-      return res.send(newBooking);
-    }
-  }
-
-  res.send('OK');
-});
-
-app.get('/bookings', (_req, res) => {
-  res.send(bookings);
-});
-
-// Favorites
-app.get('/favorites', (_req, res) => {
-  const favorites: Listing[] = listings.filter(listing => listing.favorite);
-  res.send(favorites);
-});
-
-app.post('/favorite-listing', (req, res) => {
-  const id = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      listings[i].favorite = true;
-      return res.send('Favorited.');
-    }
-  }
-});
 
 app.listen(port);
 
